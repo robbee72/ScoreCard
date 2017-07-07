18226e6b40dd4e979e14e785743d02970a3d6266
robbee72 <krobnson@gmail.com>
2017-07-07 14:41:15 -0500

    sort ranking

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,7 +1,8 @@
 class PlayersController < ApplicationController
+  helper_method :sort_column, :sort_direction
 
   def index
-    @player = Player.all
+    @player = Player.order("#{sort_column} #{sort_direction}")
   end
 
   def new
@@ -9,14 +10,15 @@ class PlayersController < ApplicationController
   end
 
   def create
+    #render plain: params[:player].inspect
     @player = Player.new(player_params)
     if @player.save
       flash[:notice] = "Player was successfully added"
-    redirect_to player_path(@player)
-  else
-    render 'new'
+      redirect_to players_path(@player)
+    else
+      render 'new'
+    end
   end
-end
 
   def show
     @player = Player.find(params[:id])
@@ -38,6 +40,18 @@ end
 
   private
   def player_params
-    params.require(:player).permit(ranking)
+    params.require(:player).permit(:name, :ranking)
+  end
+
+  def sortable_columns
+    ["name", "ranking"]
+  end
+
+  def sort_column
+    sortable_columns.include?(params[:column]) ? params[:column] : "ranking"
+  end
+
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
   end
 end
